065f0019c688142ddfd3366edab7abf4aa5e6a3b
Dima <dolix2263@gmail.com>
2022-03-30 23:06:32 +0300

    Исправлено после первого ревью

--- pages/index.js
+++ pages/index.js
@@ -2,51 +2,43 @@
 let nameAria = document.querySelector('.profile__name');
 let activityAria = document.querySelector('.profile__activity');
 let buttonEdit = document.querySelector('.profile__edit-button');
-let buttonAdd = document.querySelector('.profile__add-button');
 
 let popupElement = document.querySelector('.popup');
 let formElement = popupElement.querySelector('.form');
 let formClose = popupElement.querySelector('.popup__close');
-let nameInput = formElement.querySelector('.form__input-name');
-let activityInput = formElement.querySelector('.form__input-activity');
-let formSaveButton = formElement.querySelector('.form__button');
+let nameInput = formElement.querySelector('.form__input_value_name');
+let activityInput = formElement.querySelector('.form__input_value_activity');
+
 
 //функция добавления-удаления класса модификатора
-function popupOpenClose() {
+function openClosePopup() {
   popupElement.classList.toggle('popup_opened');
+  nameInput.value = nameAria.textContent;
+  activityInput.value = activityAria.textContent;
 }
-
-//закрытие попапа при клике вне его окна
-function clickOverleyPopup() {
-  if (event.target === event.currentTarget) {
-    popupOpenClose();
-  }
-}
-
-//считываем клики
-buttonEdit.addEventListener('click', popupOpenClose);
-formClose.addEventListener('click', popupOpenClose);
-popupElement.addEventListener('click', clickOverleyPopup);
-
-//задал значения в окна ввода формы
-nameInput.value = nameAria.textContent;
-activityInput.value = activityAria.textContent;
 
 //функция отключения отправки формы, добавления значений 
 //введенных в форму в соответствующие теги 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
-  let valueNameAria = nameInput.value;
-  let valueActivityAria = activityInput.value;
-
-  nameAria.textContent = valueNameAria;
-  activityAria.textContent = valueActivityAria;
-
-  popupOpenClose();
+  nameAria.textContent = nameInput.value;
+  activityAria.textContent = activityInput.value;
+  openClosePopup();
 }
 
+//закрытие попапа при клике вне его окна
+// function clickOverleyPopup() {
+//   if (event.target === event.currentTarget) {
+//     openClosePopup();
+//   }
+// }
+
+
+//считываем клики
+buttonEdit.addEventListener('click', openClosePopup);
+formClose.addEventListener('click', openClosePopup);
+
+// popupElement.addEventListener('click', clickOverleyPopup);
+
 //считывает событие: отпрака формы
-formElement.addEventListener('submit', formSubmitHandler); 
-
-
+formElement.addEventListener('submit', formSubmitHandler);
