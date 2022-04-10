4745a7e6a27c44f052f290032b3189aa825623fc
Dima <dolix2263@gmail.com>
2022-04-10 21:31:34 +0300

    ПР5 готова к сдаче

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 // создание переменных
+
 // переменные для кнопок и полей с информацией в profile
 const nameAria = document.querySelector('.profile__name');
 const activityAria = document.querySelector('.profile__activity');
@@ -22,16 +23,15 @@ const placeInput = popupAddElement.querySelector('.form__input_value_place');
 const linkInput = popupAddElement.querySelector('.form__input_value_link');
 
 // переменные для popup-photo
-const popupPhotoContainer = document.querySelector('.popap_type_photo');
-const popupPhotoElement = document.querySelector('.popap__photo-element');
+const popupPhotoContainer = document.querySelector('.popup_type_photo');
+const popupPhotoElement = document.querySelector('.popup__photo-element');
+const popupPhotoTitle = document.querySelector('.popup__title-photo');
+const buttonClosePopupPhoto = document.querySelector('.popup__close_type_image');
 
-
-
+// переменные для template
+const template = document.querySelector('.templateCard');
 const сardСontainer = document.querySelector('.elements__container');
 const сardElement = document.querySelectorAll('.element');
-
-const template = document.querySelector('.templateCard');
-
 const elementCardImage = document.querySelector('.element__image');
 const elementCardTitle = document.querySelector('.element__title');
 
@@ -65,30 +65,43 @@ const initialCards = [
   }
 ];
 
-// функция для добавления при загрузке страницы и удаления кнопкой карточек
+// функция выбора элементом карточки в template и добавления им слушателей событий
 function getCards(elem) {
   const newCard = template.content.cloneNode(true);
   const cardImage = newCard.querySelector('.element__image');
   const cardTitle = newCard.querySelector('.element__title');
-  
   const buttonDeletCard = newCard.querySelector('.element__delete');
-  buttonDeletCard.addEventListener('click', deletCard);
-
   const buttonLikeCard = newCard.querySelector('.element__like');
-  buttonLikeCard.addEventListener('click', getLikeCard);
- 
+  
   cardTitle.textContent = elem.name;
   cardImage.src = elem.link;
+ 
+  buttonDeletCard.addEventListener('click', deletCard);
+  buttonLikeCard.addEventListener('click', getLikeCard);
+  cardImage.addEventListener('click', openImagePopap);
+  
   return newCard;
 }
 
 // добавление карточек из массива
-function render() {
+function renderCard() {
   const cardFromArray = initialCards.map(getCards);
   сardСontainer.append(...cardFromArray);
 }
 
-render();
+renderCard();
+
+//функция создания новой карточки пользователем
+function addCardSubmit (evt) {
+  evt.preventDefault();
+  const card = getCards({name: placeInput.value, link: linkInput.value});
+  сardСontainer.prepend(card);
+  placeInput.value = '';
+  linkInput.value = '';
+  card.addEventListener('click', openClosePopup);
+
+  openClosePopup(popupAddElement);
+}
 
 // удаление карточки при нажатии на delet
 function deletCard(evt) {
@@ -102,41 +115,16 @@ function getLikeCard(evt) {
   like.classList.toggle('element__like_active');
 }
 
-// добавление карточек при помощи попап add
-function addCards() {
-  const newCard = template.content.cloneNode(true);
-  const cardImage = newCard.querySelector('.element__image');
-  const cardTitle = newCard.querySelector('.element__title');
-  const buttonDeletCard = newCard.querySelector('.element__delete');
-  buttonDeletCard.addEventListener('click', deletCard);
-  cardTitle.textContent = placeInput.value;
-  cardImage.src = linkInput.value;
-
-  return newCard;
+function openImagePopap (evt) {
+  popupPhotoElement.src = evt.target.src;
+  popupPhotoTitle.textContent = evt.target.nextElementSibling.firstElementChild.textContent;
+  popupPhotoContainer.classList.toggle('popup_opened');
 }
 
-console.log(addCards());
-
-//функция отключения отправки формы add, добавления значений 
-//введенных в форму в соответствующие теги 
-function addCardSubmit (evt) {
-  evt.preventDefault();
-  const card = addCards();
-  сardСontainer.prepend(card);
-  closePopup(popupAddElement);
+//функция открытия/закрытия Popup
+function openClosePopup(elem) {
+  elem.classList.toggle('popup_opened');
 }
-
-//функция открытия Popup
-function openPopupEdit(popupEditElement) {
-  popupEditElement.classList.add('popup_opened');
-}
-
-//Функция закрытия Popup
-function closePopup(popupEditElement) {
-  popupEditElement.classList.remove('popup_opened');
-}
-
-
 
 //функция отключения отправки формы edit, добавления значений 
 //введенных в форму в соответствующие теги 
@@ -144,49 +132,44 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   nameAria.textContent = nameInput.value;
   activityAria.textContent = activityInput.value;
-  closePopup(popupEditElement);
+  openClosePopup(popupEditElement);
 }
-
 
 //закрытие попапа при клике вне его окна
 // function clickOverleyPopup() {
 //   if (event.target === event.currentTarget) {
-//     openClosePopup();
+//     openopenClosePopup();
 //   }
 // }
-
 
 //считываем клики
 
 // открытие попап edit
 editButton.addEventListener('click', function() {
-  openPopupEdit(popupEditElement);
+  openClosePopup(popupEditElement);
   nameInput.value = nameAria.textContent;
   activityInput.value = activityAria.textContent;
 });
 
 // закрытие попап edit
 formClose.addEventListener('click', function() {
-  closePopup(popupEditElement);
+  openClosePopup(popupEditElement);
 });
 
 // открытие попап add
 addButton.addEventListener('click', function() {
-  openPopupEdit(popupAddElement);
+  openClosePopup(popupAddElement);
 });
 
 // закрытие попап add
 closeFormAdd.addEventListener('click', function() {
-  closePopup(popupAddElement);
+  openClosePopup(popupAddElement);
 });
 
-// открытие попап с картинкой
-// elementCardImage.addEventListener('click', function() {
-//   openPopupEdit(popupPhotoContainer);
-
-// });
-
-
+// закрытие попап попап-photo
+buttonClosePopupPhoto.addEventListener('click', function() {
+  openClosePopup(popupPhotoContainer);
+});
 
 // popupEditElement.addEventListener('click', clickOverleyPopup);
 
@@ -197,4 +180,4 @@ formAddElement.addEventListener('submit', addCardSubmit);
 
 
 
-console.log(сardElement.content.cloneNode(true));
+
